abecdfe4fcc686247934efbf739367392c8b86a5
michael <mludwig0626@aol.com>
2022-02-09 20:01:49 -0500

    Basic card functionality

--- frontend/EPGFrontend/src/app/app.module.ts
+++ frontend/EPGFrontend/src/app/app.module.ts
@@ -8,18 +8,22 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardlistComponent } from './components/cardlist/cardlist.component';
 import { HttpClientModule } from '@angular/common/http';
 import { EventserviceService } from './services/eventservice.service';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     NavbarComponent,
-    CardlistComponent
+    CardlistComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [EventserviceService],
   bootstrap: [AppComponent]
--- frontend/EPGFrontend/src/app/users.ts
+++ frontend/EPGFrontend/src/app/users.ts
@@ -1,4 +1,8 @@
 export interface users {
     //user variables go here
-
+    userid:number;
+    username:string;
+    userfirstName:string;
+    userlastName:string;
+    useremail:string;
 }
