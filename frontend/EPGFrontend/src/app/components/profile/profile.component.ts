de4a1e16001395259cd3575bfaa1e6acda8e5942
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-02-10 22:36:05 -0500

    testing done for preferences

--- frontend/EPGFrontend/src/app/components/profile/profile.component.ts
+++ frontend/EPGFrontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -12,9 +13,21 @@ export class ProfileComponent implements OnInit {
   userName:string="testUser";
   password:string="testpw";
   userEmail:string="test@test.com";
-  userPreferences: []=[];
+  preferences:FormGroup;
 
-  constructor() { }
+  constructor(fb:FormBuilder) { 
+    this.preferences = fb.group({
+      education:false, 
+      recreational:false,
+      social:false,
+      diy:false,
+      charity:false,
+      cooking:false,
+      relaxation:false,
+      music:false,
+      busywork:false
+    });
+  }
 
   ngOnInit(): void {
   }
