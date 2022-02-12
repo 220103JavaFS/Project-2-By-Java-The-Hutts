257e940878022d5b04383935481fe8a39f378942
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-02-11 23:23:45 -0500

    added more variables and constructor

--- frontend/EPGFrontend/src/app/users.ts
+++ frontend/EPGFrontend/src/app/users.ts
@@ -1,8 +1,22 @@
-export interface users {
+import { UserPreferences } from './user-preferences';
+
+export class users {
     //user variables go here
-    userid:number;
-    username:string;
-    userfirstName:string;
-    userlastName:string;
-    useremail:string;
+    public userId:number;
+    public firstname:string;
+    public lastname:string;
+    public username:string;
+    public email:string;
+    public password:string;
+    public userPreferences:Array<UserPreferences>;
+
+    constructor(userId:number, firstname:string, lastname:string, username:string, email:string, password:string, userPreferences:Array<UserPreferences>){
+        this.userId=userId;
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.userPreferences=userPreferences;
+    }
 }
