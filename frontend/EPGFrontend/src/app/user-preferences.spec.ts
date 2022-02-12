54879f323949934f7b4acf638dcd66eb5032775d
HanPark-Ethan <ethan.hanp89@gmail.com>
2022-02-11 23:22:18 -0500

    enum user-preference created

---
+++ frontend/EPGFrontend/src/app/user-preferences.spec.ts
@@ -0,0 +1,7 @@
+import { UserPreferences } from './user-preferences';
+
+describe('UserPreferences', () => {
+  it('should create an instance', () => {
+    expect(UserPreferences).toBeTruthy();
+  });
+});
---
+++ frontend/EPGFrontend/src/app/user-preferences.ts
@@ -0,0 +1,11 @@
+export enum UserPreferences {
+    Education = "EDUCATION",
+    Recreational = "RECREATIONAL",
+    Social = "SOCIAL",
+    Diy = "DIY",
+    Charity = "CHARITY",
+    Cooking = "COOKING",
+    Relaxation = "RELAXATION",
+    Music = "MUSIC",
+    Busywork = "BUSYWORK"
+}
